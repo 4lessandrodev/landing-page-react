739aeccb0f77165673aca8988b0ec5f6e2d41043
4lessandrodev <alessandroadm@live.com>
2021-11-15 10:07:45 -0300

    styles(fonts): set fonts

--- src/styles/index.js
+++ src/styles/index.js
@@ -1,13 +1,22 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyles = createGlobalStyle`
+
+	@import url('https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,400;0,500;0,700;1,200&display=swap');
+
 	*{
 		margin: 0;
 		padding: 0;
 	}
 
+	html {
+		font-size: 62.5%;
+	}
+
 	body{
 		background-color: #ededed;
+		font-size: 1.6rem;
+		font-family: 'Roboto Mono', monospace;
 	}
 `;
 
